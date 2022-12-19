0359edd07e51a35d447715e853b81e827e4499d8
dim-27 <dimasfirmanda27@gmail.com>
2022-12-20 02:39:41 +1100

    first commit: 1. add backend folder 2. add readme file

---
+++ backend/exercise-todo-java/src/main/java/com/dim27/exercisetodojava/ExerciseTodoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dim27.exercisetodojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseTodoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseTodoJavaApplication.class, args);
+	}
+
+}
---
+++ backend/exercise-todo-java/src/test/java/com/dim27/exercisetodojava/ExerciseTodoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dim27.exercisetodojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseTodoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
